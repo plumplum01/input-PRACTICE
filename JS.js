10e3081da75e6e84961208578336c5b983a19199
plumplum01 <plumplum010101@gmail.comL>
2024-05-04 14:38:18 +0900

    pay

--- JS.js
+++ JS.js
@@ -29,3 +29,25 @@ function selectAll(selectAll) {
         checkbox.checked = selectAll.checked;
     })
 }
+
+// 결제
+// 1. 버튼 전환클릭
+// 2. 클릭시 모달 오픈
+// 3. 닫기시 모달 클로즈
+const payHow = document.querySelectorAll('.pay-how');
+
+payHow.forEach(btn => {
+    btn.addEventListener('click', () => {
+        payHow.forEach(btn => {btn.classList.remove("pay-active-btn")});
+        btn.classList.add("pay-active-btn");
+
+        const payContainer = document.querySelector(".pay-modal-container");
+        payContainer.style.display = "flex";
+    })
+})
+
+const payClose = document.querySelector('.pay-close');
+payClose.addEventListener('click', () => {
+    const payContainer = document.querySelector(".pay-modal-container");
+    payContainer.style.display = "none";
+})
